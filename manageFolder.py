4359a73c2913bfccdd516131400e48f1f5a52c4f
kwangncp21 <157690526+kwangncp21@users.noreply.github.com>
2025-08-14 21:45:11 +0700

    report robot

--- manageFolder.py
+++ manageFolder.py
@@ -9,4 +9,3 @@ def move_png_to_success_case():
     for file in os.listdir(current_dir):
         if file.endswith(".png") and file.startswith("selenium-screenshot"):
             shutil.move(os.path.join(current_dir, file), os.path.join(success_dir, file))
-
